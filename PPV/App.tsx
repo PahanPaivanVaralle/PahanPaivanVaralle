7316c371bf50eeae65bd523a7ae9bc07e786ef76
Juha <58532349+MrJuhis@users.noreply.github.com>
2026-03-12 15:38:29 +0200

    Navigation setup

--- PPV/App.tsx
+++ PPV/App.tsx
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+import Home from "./pages/home";
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
   );
 }
 
