70866206a333451041a8f8509d7acfcbdc2b2b8f
eeromelamies <eero.mela@gmail.com>
2026-03-17 14:12:51 +0200

    UI work
    Making UI beautiful

--- PPV/global.ts
+++ PPV/global.ts
@@ -1,23 +1,28 @@
 import {StyleSheet} from "react-native";
 
+
 export const styles = StyleSheet.create({
+    imageBackground: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
     background: {
         backgroundColor: "#E4ACEA",
     },
+ 
     container: {
-        flex: 1,
-        flexDirection: 'column',
+        flex: 1,                
+        justifyContent: "flex-start", 
+        alignItems: "center",    
+        paddingTop: 50,
 
     },
     TextContainer: {
         marginTop: 50,
         alignSelf: 'center',
         width: '90%',
-        backgroundColor: '#ffbff2',
         padding: 20,
         borderRadius: 15,
-        borderWidth: 1,
-        borderColor: '#ccc',
         shadowColor: '#000',
         shadowOpacity: 0.4,
         shadowRadius: 4,
@@ -25,11 +30,17 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         columnGap: 20,
+        overflow: 'hidden',
+        backgroundColor: 'rgba(255,255,255,0.2)',
+        borderWidth: 1,
+        borderColor: 'rgba(255,255,255,0.3)',
+        
     },
     textStyle: {
         fontSize: 30,
         color: '#333',
         textAlign: 'center',
+        flexWrap: 'wrap'
     },
     imageStyle: {
         flex: 1,
@@ -39,8 +50,22 @@ export const styles = StyleSheet.create({
 
     },
     imageContainer: {
+        margin: 20,
         flexDirection: 'row',
         gap: 10,
+        padding: 20,
+        borderRadius: 15,
+        shadowOpacity: 0.4,
+        shadowRadius: 4,
+        elevation: 3,
+        alignItems: 'center',
+        justifyContent: 'center',
+        columnGap: 20,
+        overflow: 'hidden',
+        backgroundColor: 'rgba(255,255,255,0.2)',
+        borderWidth: 1,
+        borderColor: 'rgba(255,255,255,0.3)',
+        width: '90%',    
     },
     CameraContainer: {
         flex: 1,
@@ -80,4 +105,4 @@ export const styles = StyleSheet.create({
     map: {
         flex: 1,
     }
-});
+}); // värit taustalle #B5DACE, #ECC0D1
--- PPV/pages/home.tsx
+++ PPV/pages/home.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Image } from 'react-native';
-import {styles} from "../global";
+import { Text, View, Image, ImageBackground, ScrollView } from 'react-native';
+import { styles } from "../global";
 import PocketBase from 'pocketbase';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+
 
 const pb = new PocketBase('https://pocketbase.misteri.fi')
 
 export default function Home() {
 
+    const image = require("../assets/sovelluksenTausta.jpg")
     const [token, setToken] = useState("");
 
     useEffect(() => {
@@ -24,29 +26,35 @@ export default function Home() {
                 console.log("Login error:", err);
             }
         };
-
         login();
     }, []);
 
     return (
-        
-        <View style={styles.container}>
-            <View style={styles.TextContainer}>
-                <Text style={styles.textStyle}>
-                    {pb.authStore.token.toString()}
-                </Text>
-            </View>
-            <View style={styles.imageContainer}>
-                <Image
-                    source={require('../assets/cat.png')}
-                    style={styles.imageStyle}
-                />
-                <Image
-                    source={require('../assets/icon.png')}
-                    style={styles.imageStyle}
-                />
-            </View>
-            <StatusBar style="auto" />
-        </View>
+
+        <ImageBackground source={image} style={styles.imageBackground}>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.TextContainer}>
+                        <Text style={styles.textStyle}>
+                            {pb.authStore.token.toString()}
+                        </Text>
+                    </View>
+
+                    <View style={styles.imageContainer}>
+                        <Image
+                            source={require('../assets/cat.png')}
+                            style={styles.imageStyle}
+                        />
+                    </View>
+                    <View style={styles.imageContainer}>
+                        <Image
+                            source={require('../assets/icon.png')}
+                            style={styles.imageStyle}
+                        />
+                    </View>
+                    <StatusBar style="auto" />
+                </View>
+            </ScrollView>
+        </ImageBackground>
     );
 }
