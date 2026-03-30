e896d9acebb9e28913d43ff5b7d376782d1acc80
Juha <58532349+MrJuhis@users.noreply.github.com>
2026-03-30 14:42:22 +0300

    Passwordless login system

--- PPV/lib/pocketbase.ts
+++ PPV/lib/pocketbase.ts
@@ -14,3 +14,27 @@ export async function getUserID() {
   cachedUserID = await DeviceInfo.getUniqueId();
   return cachedUserID;
 }
+
+export async function Login() {
+  if (!cachedUserID) await getUserID();
+
+  const data = {
+    userid: cachedUserID,
+    username: await DeviceInfo.getDeviceName(),
+    device_name: await DeviceInfo.getDeviceName(),
+    device_manufacturer: await DeviceInfo.getManufacturer(),
+    device_os_version: DeviceInfo.getSystemName() + " " + DeviceInfo.getSystemVersion(),
+    last_ip: await DeviceInfo.getIpAddress(),
+  };
+
+  try {
+    return await pb.collection('users').create(data);
+  } catch (err) {
+    if (err.status === 400) {
+      return await pb
+        .collection('users')
+        .getFirstListItem(`userid="${cachedUserID}"`);
+    }
+    throw err;
+  }
+}
