b1638aceb57edd32ff2c75fe0ef7a37d41849b48
Juha <58532349+MrJuhis@users.noreply.github.com>
2026-04-17 12:52:14 +0300

    Fixes and optimizations

--- PPV/utils/newsSorter.ts
+++ PPV/utils/newsSorter.ts
@@ -1,74 +1,15 @@
-import { useEffect, useState } from 'react';
 import { FeedItem, parse } from 'react-native-rss-parser';
 
 export const getNews = async (): Promise<FeedItem[]> => {
-  const negativeWords = [
-    'trump',
-    'sota',
-    'venäjä',
-    'putin',
-    'moka',
-    'varoittaa',
-    'kaatui',
-    'epäilty',
-    'rikos',
-    'hinnat',
-    'pilaa',
-    'ostaa',
-    'petti',
-    'tekoäly',
-    'ongelmia',
-    'vankila',
-    'nato',
-    'ahdistuneita',
-    'epidemia',
-    'verilöyly',
-    'onnettomuus',
-    'uhri',
-    'loukkaantui',
-    'hukkui',
-    'nordea',
-    'korruptio',
-    'israel',
-    'räjähti',
-    'räjähdys',
-    'uhkaus',
-    'kuollut',
-    'oikeuteen',
-    'hengenvaara',
-    'itsetuhoisen',
-    'iran',
-    'porno',
-    'murtauduttu',
-    'pelkää',
-    'kuoli',
-    'epäillään',
-    'uhattuna',
-    'tappaa',
-    'epstein',
-    'sotilas',
-    'murhasi',
-    'sortui',
-    'vallankaappaus',
-    'poliisi pysäytti',
-    'vallan',
-    'tarjottu seksistä',
-  ];
-
   try {
-    const sources = ['https://www.goodnewsnetwork.org/category/news/feed/'];
-
-    const feeds = await Promise.all(
-      sources.map(async (url) => {
-        const res = await fetch(url);
-        const text = await res.text();
-        const feed = await parse(text);
-
-        return feed.items;
-      }),
+    const res = await fetch(
+      'https://www.goodnewsnetwork.org/category/news/feed/',
     );
 
-    return feeds.flat();
+    const text = await res.text();
+    const feed = await parse(text);
+
+    return feed.items;
   } catch (err) {
     console.log('News error:', err);
     return [];
